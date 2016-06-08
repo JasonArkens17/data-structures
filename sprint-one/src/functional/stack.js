49643354f9c74563cdbc986c0ae813ddffcc3b86
JasonArkens17 <jason.arkens17@gmail.com>
2016-06-08 15:01:09 -0700

    Solve stack for functional pattern

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var index = 0; 
 
   // Implement the methods below
   someInstance.push = function(value) {
+    index++; 
+    storage[index] = value;
   };
 
   someInstance.pop = function() {
+    var value = storage[index]; 
+    delete storage[index]; 
+    if (index > 0) {
+      index--; 
+    }
+    // index = Math.max(0, index - 1); 
+    return value;
   };
 
   someInstance.size = function() {
+    return index;
   };
 
   return someInstance;
