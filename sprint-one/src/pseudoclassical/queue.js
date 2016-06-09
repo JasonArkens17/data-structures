65b0a75f740088a74e8cbb27700a243a3f526d7d
JasonArkens17 <jason.arkens17@gmail.com>
2016-06-08 20:29:42 -0700

    Solve stack and queue for pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.newestIndex = 1; 
+  this.oldestIndex = 1; 
 };
 
+Queue.prototype.size = function() {
+  return this.newestIndex - this.oldestIndex; 
+};
+
+Queue.prototype.enqueue = function(value) {
+  this[this.newestIndex] = value; 
+  this.newestIndex++; 
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.oldestIndex !== this.newestIndex) {
+    this.result = this[this.oldestIndex]; 
+    delete this[this.oldestIndex]; 
+    this.oldestIndex++; 
+    return this.result; 
+  }
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.index = 0;
+
 };
 
 
+Stack.prototype.push = function(value) {
+  this.index++;
+  this[this.index] = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.index > 0) {
+    this.result = this[this.index]; 
+    delete this[this.index]; 
+    this.index--; 
+    return this.result; 
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.index;
+};
